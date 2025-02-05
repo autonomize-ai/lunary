bfe044cd4cface8b59f661e9ad34198e8454ab7a
hgarg96 <harshita@autonomize.ai>
2025-02-05 11:37:27 +0530

    fix workspace issues

--- packages/frontend/load-env.sh
+++ packages/frontend/load-env.sh
@@ -9,10 +9,13 @@ if [ -f .env ]; then
   set +o allexport
 fi
 
-
+# Check if API_URL is set, if not, exit with an error
 if [ -z "$API_URL" ]; then
-  echo "Error: API_URL not set. Please set the API_URL environment variables."
-  exit 1
+    echo "Error: API_URL not set. Please set the API_URL environment variables."
+    exit 1
 fi
+
+# Optional: Additional environment variable checks or setup
+echo "API_URL is set to: $API_URL"
 
 LC_ALL=C  find .next -type f -exec perl -pi -e "s|xyzPLACEHOLDERxyz|${API_URL}|g" {} +
